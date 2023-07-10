c4493c0abb0450206785ed31d516c6882abbd3ea
Yi Qing <qingyi.tss@gmail.com>
2023-07-10 15:38:53 +0800

    add gzip.

--- main.go
+++ main.go
@@ -7,15 +7,18 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	logrus.SetLevel(logrus.DebugLevel)
+	
 	router := gin.Default()
+	router.Use(gzip.Gzip(gzip.DefaultCompression))
 	router.LoadHTMLGlob("templates/*")
-
+	
 	server.AddRouter(router)
 	router.Static("/static", "./static")
 	router.StaticFile("/favicon.ico", "./static/favicon.ico")
